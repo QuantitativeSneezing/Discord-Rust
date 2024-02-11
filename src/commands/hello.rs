7d799efe20c795c65eb165304f4c421e1ea57bd5
Jason Arnold <jatsburgern@gmail.com>
2024-02-10 20:29:48 -0500

    commented out message variable accidentally

--- src/commands/hello.rs
+++ src/commands/hello.rs
@@ -18,7 +18,7 @@ pub async fn run(ctx: &Context, command: &ApplicationCommandInteraction) {
     let test_var = vec!["HELLO!!!!!!!!!!", "WASSUP", "HI", "GREETINGS", "HAI DOMO"];
     // let mut rng = rand::thread_rng();
     // rng.gen::<i64>()
-    // let message= test_var[0];
+    let message= test_var[0];
     if let Err(e) = command.create_interaction_response(
         &ctx,
         |r| {
