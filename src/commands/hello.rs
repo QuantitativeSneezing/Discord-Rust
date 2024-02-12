d57ce4c255ee73871dd3c78252d840e4ea1f5d9a
Jason Arnold <jatsburgern@gmail.com>
2024-02-12 16:36:35 -0500

    added rng to hello command

--- src/commands/hello.rs
+++ src/commands/hello.rs
@@ -1,9 +1,9 @@
-// use rand::{
-//     Rng,
-//     thread_rng,
-//     seq::SliceRandom,
-
-// };
+use rand::{
+    // Rng,
+    // seq::SliceRandom,
+    rngs::StdRng
+};
+// use std::i8::rem_euclid
 use serenity::{
     builder::CreateApplicationCommand,
     client::Context,
@@ -14,8 +14,13 @@ use serenity::{
 };
 
 pub async fn run(ctx: &Context, command: &ApplicationCommandInteraction) {
-    let test_var = vec!["HELLO!!!!!!!!!!", "WASSUP", "HI", "GREETINGS", "HAI DOMO"];
-    let message= test_var[0];
+    let possible_greetings = vec!["HELLO!!!!!!!!!!", "WASSUP", "HI", "GREETINGS", "HAI DOMO"];
+    let mut random = rand::random::<i8>();
+    if random < -1 {
+        random = random * -1;
+    }
+    random= random % 5;
+    let message= possible_greetings[random as usize];
     if let Err(e) = command.create_interaction_response(
         &ctx,
         |r| {
